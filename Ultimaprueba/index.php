7c15862be27ce1387710bf831417cb33954e0d12
Alumno <Alumno@W7Bsica>
2017-10-17 14:24:38 +0200

    nuevo

--- Ultimaprueba/index.php
+++ Ultimaprueba/index.php
@@ -1,3 +1,10 @@
 <?php
-echo "<h1>funciona</h1>";
+$nombre1="ming";
+$nombre2="manu";
+echo "<h1>Funciona esta pagina de maravilla, ATENTO me presento llego el momento somos $nombre1 $nombre2</h1>";
+
+define("alumno", "manu");
+
+echo "<p> el alumno es " .alumno. "</p>";
+
 ?>
