5cdfad064fd348d7a31d76d9189c9e0e6a7cad20
Alumno <Alumno@W7Bsica>
2017-10-25 12:38:58 +0200

    ola

--- PHP U2P03-PHP-Control de flujo/ecf-meses.php
+++ PHP U2P03-PHP-Control de flujo/ecf-meses.php
@@ -1,1 +1,33 @@
+<html>
+<head></head>
+<body>
+<form action="ecf-recorte.php" method="post">
+Introduce A: <input type="text" name="A">
+<input type="submit" name="Enviar">
+</form>
 <?php
+if(isset($_POST["Enviar"])){
+    $a=$_POST['A'];
+    $r=0;
+    $c=1;
+while ($c<=strlen($a)){
+    
+    
+        for ($r;$r<=(strlen($a)-$c);$r++){
+            Echo $a[$r];      
+        }
+        echo"<p></p>";
+        $c++;
+        $r=0;
+        
+}
+   
+    
+   
+    
+}else
+    echo"<p1>Introduce los datos necesarios</p1>";
+    
+    ?>
+</body>
+</html>
