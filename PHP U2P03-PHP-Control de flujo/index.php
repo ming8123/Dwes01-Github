2d6df9979a6e41e2cfc5bbd1d00f9e97bcde3a91
Alumno <Alumno@W7Bsica>
2017-10-31 12:56:11 +0100

    nuevo

--- PHP U2P03-PHP-Control de flujo/index.php
+++ PHP U2P03-PHP-Control de flujo/index.php
@@ -1,16 +1,16 @@
 <html>
-<body>
+<body style="color: blue;background-color:yellow;" >
 
-<a href="ecf-diferencia.php">*ecf-diferencia*</a>
-<a href="ecf-factorial.php">*ecf-factorial*</a>
-<a href="ecf-meses.php">*ecf-meses*</a>
-<a href="ecf-multiplicacion.php">*ecf-multiplicacion*</a>
-<a href="ecf-potencia.php">*ecf-potencia*</a>
-<a href="ecf-recorte.php">*ecf-recorte*</a>
-<a href="ecf-suma.php">*ecf-suma*</a>
-<a href="ecf-multiplos.php">*ecf-multiplos*</a>
-<a href="ecf-acumulador.php">*ecf-acumulador*</a>
-<a href="ecf-cuadrado.php">*ecf-cuadrado*</a>
+<p><a href="ecf-diferencia.php">*ecf-diferencia*</a></p>
+<p><a href="ecf-factorial.php">*ecf-factorial*</a></p>
+<p><a href="ecf-meses.php">*ecf-meses*</a></p>
+<p><a href="ecf-multiplicacion.php">*ecf-multiplicacion*</a></p>
+<p><a href="ecf-potencia.php">*ecf-potencia*</a></p>
+<p><a href="ecf-recorte.php">*ecf-recorte*</a></p>
+<p><a href="ecf-suma.php">*ecf-suma*</a></p>
+<p><a href="ecf-multiplos.php">*ecf-multiplos*</a></p>
+<p><a href="ecf-acumulador.php">*ecf-acumulador*</a></p>
+<p><a href="ecf-cuadrado.php">*ecf-cuadrado*</a></p>
 
 
 
